38823fc55d8ed2b12eddcc3303a3a57b74284cc0
Blackstar <baker644@umn.edu>
2020-06-01 23:02:53 -0500

    Code is fully functional, MVP reached

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -35,7 +37,7 @@ const choicesQuery = [
                 short: "Generate HTML",
             },
             {
-                name: "Exit application without generating page.  ",
+                name: "Exit application without generating page.",
                 value: "abort",
                 short: "Exit Application",
             },
@@ -132,68 +134,40 @@ const internQuery = [
 
 function userPrompt(prompt) {return inquirer.prompt(prompt);}
 
-async function generateTeamPrompt() {
+async function generateTeam() {
     try {
-        console.log("This is a welcome statement");
+        console.log("\nThis is a welcome statement\n");
         const manager = await userPrompt(managerQuery);
         employees.push(new Manager(...Object.values(manager)));
-
+        console.log("")
         for (employee of employees) {
             let { choice } = await userPrompt(choicesQuery);
-            console.log(choice);
+            console.log("")
             switch (choice) {
                 case "addEngineer":
                     let engineer = await userPrompt(engineerQuery);
                     employees.push(new Engineer(...Object.values(engineer)));
+                    console.log("")
                     break;
                 case "addIntern":
                     let intern = await userPrompt(internQuery);
                     employees.push(new Intern(...Object.values(intern)));
+                    console.log("")
                     break;
                 case "finish":
-                    console.log(employees);
-                    console.log("Wooooo you did it!")
+                    console.log("Employee entry successfully completed")
                     break;
                 case "abort":
                     console.log("Application terminated by the user");
                     return;
             } 
         }
-
-        console.log ("This will only print if the loop is exited")
+        const htmlString = render(employees);
+        await writeFileAsync(outputPath, htmlString);
+        console.log(`\nYour document, team.html has been successfully generated!`)
     } catch(err) {
         console.log(err);
     }
 }
 
-generateTeamPrompt();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-
-
-
-//Use the Inquirer npm package to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
-//Your app will run as a Node CLI to gather information about each employee.
+generateTeam();
